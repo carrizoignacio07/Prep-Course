62ccff2e97b072e1945242d60131bfb0a38c9688
unknown <carrizoignacio96@gmail.com>
2022-03-31 18:42:18 -0300

    arreglos var-resto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -104,7 +104,7 @@ function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
   var resto = x / y;
-  return var;
+  return resto;
 }
 
 function esPar(num) {
