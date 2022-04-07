8ca6710b6a4a8dadfffe663d51e3d91b503c2afb
unknown <carrizoignacio96@gmail.com>
2022-04-07 20:17:22 -0300

    arreglo en colors switch

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -66,17 +66,25 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-  if (color == "blue") {
-    return "This is blue";
-  } else if (color == "red") {
-    return "this is red";
-  } else if (color == "green") {
-    return "this is green";
-  } else if (color == "orange") {
-    return "this is orange";
-  } else {
+  switch (color) {
+    case "blue":
+      return "This is blue";
+      break;
+    case "red"
+      return "this is red";
+      break;
+    case "green"
+      return "this is green";
+      break;
+    case "orange"
+      return "this is orange";
+      break;
+    default
     return "Color not found";
+    }
   }
+
+
 }
 
 function esDiezOCinco(numero) {
