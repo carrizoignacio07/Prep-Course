b3584e9b97fc433db135ba0fa8f7916cacfa18c3
unknown <carrizoignacio96@gmail.com>
2022-04-08 22:07:41 -0300

    arreglos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -130,7 +130,9 @@ function fizzBuzz(numero) {
     return "fizz";
   } else if (numero % 5 === 0) {
     return "buzz";
-  } else {
+  } else if (numero % 5 === 0|| numero % 3 === 0) {
+    return "fizzbuzz";
+  } else{
     return numero;
   }
 }
@@ -143,7 +145,7 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
   if (num1 > num2 || num1 > num3 || num1 > 0) {
-    return "Numero 1 es mayor y positivo";
+    return "Número 1 es mayor y positivo";
   } else if (num1 < 0 && num2 < 0 && num3 < 0) {
     return "Hay negativos";
   } else if (num3 > num2 || num3 > num1) {
