622bb2637711b6ec17f82197eea7ed36d13a2e33
unknown <carrizoignacio96@gmail.com>
2022-04-13 20:10:40 -0300

    error test, posibles llaves

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -85,7 +85,6 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
-  var numeros = 
 }
 
 
