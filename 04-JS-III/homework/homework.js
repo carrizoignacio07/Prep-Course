b7b9d52ce655d29b7df02aae9bb79f24f95ed4a7
unknown <carrizoignacio96@gmail.com>
2022-04-23 17:51:37 -0300

    arreglo ejercicio mayor a cien

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -214,6 +214,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  for (var i = 0; i < array.length; i++) {
+    var mayor = [];
+    if (array[] > 100) {
+      mayor[] = array;
+    }
+    return mayor;
+  }
 }
 
 
@@ -225,6 +232,9 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  for (var i = 0; i < 10; i++) {
+    numero = numero + 2; 
+  }
 }
 
 
