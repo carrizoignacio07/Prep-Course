7ff241e8f97b9590f5f48eda27082cf4736a61b7
unknown <carrizoignacio96@gmail.com>
2022-04-25 15:46:11 -0300

    resolucion 2-3 ejercicios

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -22,6 +22,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto["property"] = null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,6 +31,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto.metodo;
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
