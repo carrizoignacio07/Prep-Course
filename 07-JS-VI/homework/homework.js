38875434046cf06b39a2e8921f9d4f9c4bf6a551
unknown <carrizoignacio96@gmail.com>
2022-05-01 13:53:08 -0300

    arreglo ejercicio 1/7: return nombre.capitalize

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,9 +4,7 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  var nuevoNombre = nombre.map(function(nombre,indice){
-    return nombre.capitalize();
-  })
+  return nombre.capitalize();
 }
 
 function invocarCallback(cb) {
