5cc3ac68115ba34139963570a76b37c9405985e6
unknown <carrizoignacio96@gmail.com>
2022-05-03 15:42:28 -0300

    solucion ejercicios 4, 5, 6 y 7

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -34,15 +34,38 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-  numeros.forEach(function(){
 
-  })
+  // Primera opcion
+  // var numeros = [1,2,3,4,5]
+  // var suma = 0;
+  // for(var i = 0; i < numeros.length; i++) {
+  //   suma = suma + numeros[i];
+  // }
+  // cb(suma);
+  
+  var sumaTotal = numeros.reduce(function(acc, curr) {
+    return acc + curr;
+  },0);
+  cb(sumaTotal);
+
+  //  numeros.forEach(function(){
+  //  cb(numeros);
+  //  })
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  
+  for(var i = 0; i < array.length; i++) {
+    cb(array[i]);
+  }
+
+  //No tenia idea, crei que tenia que usar el forEach pero no sabia como.
+  //Me cuesta mucho este tema, mas que el prototype; voy a tener que practicar mucho.
+  // array.forEach(function (cb) {
+  // })
 }
 
 function map(array, cb) {
@@ -50,12 +73,38 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+
+  var nuevoArray = array.map(function(el) {
+    return cb(el);
+  });
+  return nuevoArray;
+
+  // var array = [1,2,3,4,5]
+  // function cb (e) {console.log(e)}
+  // nuevoarray = []
+  // var nuevoArray = [];
+  // for(var i = 0; i < array.length; i++) {
+  //   nuevoArray.push(cb(array[i]));
+  //   nuevoArray[i] = cb(array[i]);
+  // }
+
+  //No logro entender que tengo que hacer, o como.
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+
+  var nuevoArray = [];
+  for(let i = 0; i<array.length; i++) {
+    if(array[i][0] === "a") {
+      nuevoArray.push(array[i])
+    }
+  }
+  return nuevoArray;
+
+  //No logro entender que tengo que hacer, o como.
 }
 
 // No modificar nada debajo de esta línea
